60db57bfe1b1c84f48dbd201d7a91f131ff8a5e4
Andrew Kelley <superjoe30@gmail.com>
2010-05-06 13:10:11 -0700

    madwave is working and needs to improve its speed now

--- madwave.py
+++ madwave.py
@@ -1,47 +1,77 @@
 import mad
-from PIL import Image
+from PIL import Image, ImageDraw
+import struct
 
 import sys
 
 image_width = 800
 image_height = 100
 
-
 madfile = mad.MadFile(sys.argv[1])
-total_samples = madfile.total_time() / 1000.0 * madfile.samplerate()
-buf = madfile.read()
-mfoffset = 0
 
-mp3min = 0.0
-mp3max = 255.0
-mp3range = float(mp3max - mp3min)
-
-image = Image.new("RGBA", (image_width, image_height), (255, 255, 255, 0))
-x = 0
-while x < image_width:
-    # what index in the mp3 buffer would this translate to?
-    sample_index = int((x / float(image_width)) * total_samples)
-    
-    # what's the sample value?
-    while sample_index - mfoffset >= len(buf):
-        buf = madfile.read()
-        if buf is None:
-            break
-        mfoffset += len(buf)
-
+# load the entire thing into memory
+bytes = ""
+while True:
+    buf = madfile.read()
     if buf is None:
         break
+    for i in range(len(buf)):
+        bytes += buf[i]
 
-    value = ord(buf[sample_index - mfoffset])
+print "done loading"
+
+channel_count = madfile.mode()
+sample_size = 2
+frame_size = sample_size * channel_count
+byte_count = len(bytes)
+frame_count = byte_count / frame_size
+#byte_count = frame_count * frame_size
+
+sample_min = -pow(2, 8*sample_size-1)+1
+sample_max = pow(2, 8*sample_size-1)
+sample_range = float(sample_max - sample_min)
+
+frames_per_pixel = frame_count / float(image_width)
+
+image = Image.new("RGBA", (image_width, image_height), (255, 255, 255, 0))
+draw = ImageDraw.Draw(image)
+
+# for each pixel
+x = 0
+while x < image_width:
+    # range of frames that fit in this pixel
+    start = int(x * frames_per_pixel)
+    end = int((x+1) * frames_per_pixel)
+    
+    # get the min and max of this range
+    min = sample_max
+    max = sample_min
+    # for each frame from start to end
+    i = start
+    while i < end:
+        offset = i * frame_size
+        lvalue = struct.unpack("h", bytes[offset:offset+sample_size])[0]
+        offset += sample_size
+        rvalue = struct.unpack("h", bytes[offset:offset+sample_size])[0]
+        value = (lvalue + rvalue) / 2.0
+
+        if value < min:
+            min = value
+        if value > max:
+            max = value
+
+        i += 1
 
     # translate into y pixel coord
-    y = int(round((value - mp3min) / mp3range * (image_height-1)))
+    y_min = int(round((min - sample_min) / sample_range * (image_height-1)))
+    y_max = int(round((max - sample_min) / sample_range * (image_height-1)))
 
     # draw
-    print "x=%s, y=%s" % (x,y)
-    image.putpixel((x, y), (0, 0, 0, 255))
+    draw.line(((x, y_min), (x, y_max)), (0,0,0,255))
 
     # iterate
     x += 1
 
 image.save("out.png")
+
+
